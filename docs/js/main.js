98e7f984befe53f0b4b27b5d1607bfb6094fe278
Daniil A. <foxones80@gmail..com>
2024-06-08 00:11:46 +0300

    fixed some

--- docs/js/main.js
+++ docs/js/main.js
@@ -5557,16 +5557,23 @@ __webpack_require__.r(__webpack_exports__);
 window.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.hero__images');
   const frameCount = 400;
-  const currentFrame = index => `./../img/animation/pic_${index.toString().padStart(5, '0')}.webp`;
+  const currentFrame = index => `./img/animation/pic_${index.toString().padStart(5, '0')}.webp`;
   const arrImages = [];
   for (let i = 0; i < frameCount; i++) {
-    arrImages.push(`./../img/animation/pic_${i.toString().padStart(5, '0')}.webp`);
+    arrImages.push(`./img/animation/pic_${i.toString().padStart(5, '0')}.webp`);
   }
+  const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+      const img = new Image();
+      img.src = arrImages[i];
+    }
+  };
   let frameIndex = 0;
   function draw() {
     if (frameIndex === frameCount) frameIndex = 0;
     container.src = arrImages[frameIndex++];
   }
+  preloadImages();
   var interval = setInterval(() => {
     draw();
   }, 40);
